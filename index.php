0f5936e29d056dec0c089e4a3ae3a10bc587a7ba
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-21 11:21:56 +0200

    trecia uzduotis komentarai

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
-<?php
-Date_default_timezone_set('Europe/Vilnius');
-?>
 <html>
     <head>
-        <title>PHP lydes ir <?php print date('Y-m-d', strtotime('+1 day'))?></title>
+        <title>Komentarai</title>
     </head>
     <body>
-        <h1>Ernestas - PHP su manim buvo ir <?php print date('H:i', strtotime('-1 hour')) . ' valanda'?> </h1>
-        <p><?php print date('Y', strtotime('+1 year')); ?> ne uz kalnu!</p>
+        <div>
+            <h1>As pradedu</h1>
+            <h2>O as papildau</h2>
+        </div>
+        <div>
+            <p>As esu ta pana, kuri kalba ir kalba ir kalba...</p>
+        </div>
     </body>
 </html>
 
