ae19857fdffa2d9f10bb9737f5c1ba2350c43f52
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-21 10:29:32 +0200

    pirmas commit pirma uzduotis + gitignore

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
     <head>
-        <title>As, pirmadienis ir PHP</title>
+        <title>As, <?php print date('D') ?> ir PHP</title>
     </head>
     <body>
         <h1>Ernestas - HTML ir PHP asas jau nuo <?php print date('Y') ?> metu</h1>
-        <p>Viska prasidejo <?php print date('m') ?> menesio, <?php print date('d') ?> diena! </p>
+        <p>Viska prasidejo <?php print date('m') ?> menesio, <?php print date('d') ?> diena!</p>
     </body>
 </html>
 
