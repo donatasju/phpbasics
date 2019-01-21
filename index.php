1fda9326c96bd5f2240ea13ae93849bd81c5177d
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-21 12:07:30 +0200

    Strtotime + random uzduotis fix

--- index.php
+++ index.php
@@ -5,11 +5,11 @@ $random2 = rand(2,12);
 ?>
 <html>
     <head>
-        <title>PHP lydes ir <?php print date('Y', strtotime('+'.($random).'year'))?></title>
+        <title>PHP lydes ir <?php print date('Y', strtotime('+' . $random . 'year'));?></title>
     </head>
     <body>
         <h1>Galbut turesiu <?php print $vaikai?> vaiku!</h1>
-        <p>D. Trumpas nebebus <?php print date('Y-m-d', strtotime('+'.($random2).'year'))?> </p>
+        <p>D. Trumpas nebebus <?php print date('Y-m-d', strtotime('+' . $random2 . 'year'));?> </p>
     </body>
 </html>
 
