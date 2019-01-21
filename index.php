99d3f39f592a86fec51ba72cfe7f06b8bb79fe7e
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-21 10:46:01 +0200

    pirma uzduotis koreguota + gitignore

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <html>
     <head>
-        <title>As, <?php print date('D') ?> ir PHP</title>
+        <title> <?php print 'As, ' . date('D') . ' ir PHP' ?></title>
     </head>
     <body>
-        <h1>Ernestas - HTML ir PHP asas jau nuo <?php print date('Y') ?> metu</h1>
-        <p>Viska prasidejo <?php print date('m') ?> menesio, <?php print date('d') ?> diena!</p>
+        <h1>Ernestas - HTML ir <?php print 'PHP' ?> asas jau nuo <?php print date('Y') . ' metu' ?></h1>
+        <p>Viska prasidejo <?php print date('m') . ' menesio' . ', ' . date('d') . ' diena' ?>!</p>
     </body>
 </html>
 
