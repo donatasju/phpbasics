a2d02c237a9d9cc7eb352bdf7431b8b10b5177a1
donatasju <donatas.jurkusl@gmail.com>
2019-01-21 11:04:08 +0200

    trys

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
+<?php
+Date_default_timezone_set('Europe/Vilnius');
+$time = date('l', strtotime('tomorrow'));
+$hour = date('H.i', strtotime('-1 hour'));
+$year = date('Y', strtotime('+1 year'))
+?>
 <html>
     <head>
-        <title><?php print 'As, ' . date('l') . " ir PHP" ?></title>
+        <title>PHP lydes ir <?php print $time ?></title>
     </head>
     <body>
-        <h1>Donatas, HTML <?php print ' ir php' ?> asas jau nuo <?php print date('Y') . ' metu' ?></h1>
-        <p>Viskas prasidejo nuo <?php print date('F') . ' menesio, ' . date("l") . ' diena.' ?></p>
+        <h1>Donatas - PHP su manim buvo ir <?php print $hour ?></h1>
+        <p><?php print $year ?> ne uz kalnu !</p>
     </body>
 </html>
