1265f0de477dacfcf62b7fc3faa7b0e8f9798ec9
donatasju <donatas.jurkusl@gmail.com>
2019-01-21 12:02:14 +0200

    last

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
+<?php
+Date_default_timezone_set('Europe/Vilnius');
+$rand_date = rand(1, 10);
+$rand = date('Y.m.d', strtotime("+$rand_date" . 'year'));
+$rand_max = rand(2, 12);
+$rand_last = date('Y.m.d', strtotime("+$rand_max" . 'year'));
+?>
+
 <html>
     <head>
-        <title>Komentarai</title>
+        <title>PHP lydes ir <?php print $rand ?> </title>
     </head>
     <body>
-        <!--Pridetas h1 ir h2 tagai-->
-        <div>
-            <h1>As pradedu</h1>
-            <h2>O as papildau</h2>
-        </div>
-    <!--<div>
-            <p>As esu ta pana, kuri kalba ir kalba, ir kalba, ir kalba ...</p>
-        </div>-->
+        <h1>Donatas - galbut turesiu <?php print rand(1, 5) ?> vaiku(us) !</h1>
+        <p>D. Trump'as nebebus prezidentu <?php print $rand_last ?></p>
     </body>
 </html>
