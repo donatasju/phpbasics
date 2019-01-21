2c3dc63346e0825cfe4aa05c080bb0b79412be68
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-21 12:11:27 +0200

    Strtotime + random uzduotis fix + tvarkingi variable

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
-$random = rand(0,10);
-$vaikai = rand(1,5);
-$random2 = rand(2,12);
+date_default_timezone_set('Europe/Vilnius');
+$random_metai = rand(0,10);
+$random_vaikai = rand(1,5);
+$random_trumpo_metai = rand(2,12);
 ?>
 <html>
     <head>
-        <title>PHP lydes ir <?php print date('Y', strtotime('+' . $random . 'year'));?></title>
+        <title>PHP lydes ir <?php print date('Y', strtotime('+' . $random_metai . 'year'));?></title>
     </head>
     <body>
-        <h1>Galbut turesiu <?php print $vaikai?> vaiku!</h1>
-        <p>D. Trumpas nebebus <?php print date('Y-m-d', strtotime('+' . $random2 . 'year'));?> </p>
+        <h1>Galbut turesiu <?php print $random_vaikai?> vaiku!</h1>
+        <p>D. Trumpas nebebus <?php print date('Y-m-d', strtotime('+' . $random_trumpo_metai . 'year'));?> </p>
     </body>
 </html>
 
