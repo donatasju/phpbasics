8078faa51720f50f675c42e7ad19ea63903655d0
donatasju <donatas.jurkusl@gmail.com>
2019-01-21 12:54:34 +0200

    done

--- index.php
+++ index.php
@@ -1,21 +1,9 @@
-<?php
-Date_default_timezone_set('Europe/Vilnius');
-$rand_number = rand(1, 10);
-$rand_titledate = date('Y.m.d', strtotime("+$rand_number" . 'year'));
-$rand_number2 = rand(2, 12);
-$rand_trumpdate = date('Y.m.d', strtotime("+$rand_number2" . 'year'));
-$rand_color = rand(0, 255);
-$rand_color1 = rand(0, 255);
-$rand_color2 = rand(0, 255);
-$rand_h1_size = rand(0, 50);
-?>
-
 <html>
     <head>
-        <title>PHP lydes ir <?php print $rand_titledate ?> </title>
+        <title></title>
+        <link rel="stylesheet" href="style.css">
     </head>
-    <body style="background-color: rgb(<?php print "$rand_color,$rand_color1,$rand_color2" ?>)">
-        <h1 style="font-size: <?php print $rand_h1_size . 'px' ?>">Donatas - galbut turesiu <?php print rand(1, 5) ?> vaiku(us) !</h1>
-        <p style="color: rgb(<?php print "$rand_color2,$rand_color,$rand_color1"?>">D. Trump'as nebebus prezidentu <?php print $rand_trumpdate ?></p>
+    <body class="img<?php print rand(1, 6) ?>">
+
     </body>
 </html>
