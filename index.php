b548d1db5a505a520d7c489cc0f37f3540a8ff83
donatasju <donatas.jurkusl@gmail.com>
2019-01-21 11:36:48 +0200

    html kom

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
         <title>Komentarai</title>
     </head>
     <body>
-        <div> <!--Pridetas h1 ir h2 tagai-->
+        <!--Pridetas h1 ir h2 tagai-->
+        <div>
             <h1>As pradedu</h1>
             <h2>O as papildau</h2>
         </div>
-<!--        <div>
+    <!--<div>
             <p>As esu ta pana, kuri kalba ir kalba, ir kalba, ir kalba ...</p>
         </div>-->
     </body>
