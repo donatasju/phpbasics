889e72337c8e547d63d41a26cceb83471c4a2869
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-21 13:54:29 +0200

    bomba uzduotis

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php
-$random_skaicius = rand(1,6);
+$sekunde = date("s");
+$sekunde_kart_10 = $sekunde * 10;
 ?>
 <html>
     <head>
-        <title>Alkoholio randomaizeris</title>
+        <title>Bomba</title>
         <link rel="stylesheet" href="css/main.css">
+        <style>
+            .klase-1 {
+                background-image: url("../images/bomb.jpg");
+                width: <?php print $sekunde_kart_10 ?>px;
+                height: <?php print $sekunde_kart_10 ?>px;
+                background-size: contain;
+                background-repeat: no-repeat;
+            }
+        </style>
     </head>
     <body>
-        <div class="<?php print "klase-$random_skaicius" ?>"></div>
+        <div class="klase-1"></div>
+        <div><?php print $sekunde?></div>
     </body>
 </html>
