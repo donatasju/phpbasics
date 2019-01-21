299565f548eb147fd6a2b7daf1be5ca3d750516c
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-21 10:24:04 +0200

    pirmas commit pirma uzduotis

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>As, pirmadienis ir PHP</title>
+    </head>
+    <body>
+        <h1>Ernestas - HTML ir PHP asas jau nuo <?php print date('Y') ?> metu</h1>
+        <p>Viska prasidejo <?php print date('m') ?> menesio, <?php print date('d') ?> diena! </p>
+    </body>
+</html>
+
+
+
