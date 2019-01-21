9ddaf010d2d69126ea8f6b02ba66a55348c92188
donatasju <donatas.jurkusl@gmail.com>
2019-01-21 13:02:01 +0200

    ahaha

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title></title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="/css/style.css">
     </head>
     <body class="img<?php print rand(1, 6) ?>">
 
