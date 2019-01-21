3561143c3ff33a5472ae0a05615d5ca16219ee95
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-21 11:36:31 +0200

    komentaras uzkomentintas fix

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
         <title>Komentarai</title>
     </head>
     <body>
-<!--        Sitas blogas paaiskina zemiau esanti diva-->
+        <!-- Sitas blogas paaiskina zemiau esanti diva -->
         <div>
             <h1>As pradedu</h1>
             <h2>O as papildau</h2>
         </div>
-<!--        <div>
+        <!--<div>
             <p>As esu ta pana, kuri kalba ir kalba ir kalba...</p>
         </div>-->
     </body>
