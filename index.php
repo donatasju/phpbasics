60d14a81cd052a39519228a827b7ff08e51f351d
donatasju <donatas.jurkusl@gmail.com>
2019-01-21 10:33:12 +0200

    pirmas

---
+++ index.php
@@ -0,0 +1,3 @@
+<title><?php print 'As, ' . date("l") . " ir PHP" ?></title>
+<h1>Donatas, HTML <?php ' ir php'?> asas jau nuo <?php print date("Y") . ' metu'?></h1>
+<p>Viskas prasidejo nuo <?php print date('F') . ' menesio, ' . date("l") . ' diena.'?></p>
