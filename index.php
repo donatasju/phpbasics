86c28d9b9b5a63b5f4791c9327e0f8d36c9e41a4
donatasju <donatas.jurkusl@gmail.com>
2019-01-21 13:54:31 +0200

    yyy

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
+<?php 
+$second = date('s') * 10;
+
+?>
 <html>
     <head>
         <title></title>
-        <link rel="stylesheet" href="/css/style.css">
+        <link rel="stylesheet" href="style/style.css">
+        <style>
+            .img {
+                background-image: url("../images/bomb.png");
+                background-repeat: no-repeat;
+                background-size: contain;
+                width: <?php print $second; ?>px;
+                height: <?php print $second; ?>px;
+            }
+        </style>
     </head>
-    <body class="img<?php print rand(1, 6) ?>">
-
+    <body>
+        <div class="img"></div>
+        <div><?php print date('s') ?></div>
     </body>
 </html>
