20f7cc4ccfbbd2aa6f796cc6d42b7df7e73a4829
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-21 11:32:50 +0200

    komentaras

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
         <title>Komentarai</title>
     </head>
     <body>
+<!--        Sitas blogas paaiskina zemiau esanti diva-->
         <div>
             <h1>As pradedu</h1>
             <h2>O as papildau</h2>
         </div>
-        <div>
+<!--        <div>
             <p>As esu ta pana, kuri kalba ir kalba ir kalba...</p>
-        </div>
+        </div>-->
     </body>
 </html>
 
