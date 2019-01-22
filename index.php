8f665fb15b42edfe116ced5ed1568296080e9c3f
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-22 11:41:30 +0200

    antra uzduotis nuotrauka 4 kart su php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
-$string = 'Labas';
-$integer = 123;
-$float = 1.5;
-$boolean = true;
-$null = null;
-
-print "$string<br>";
-print "$integer<br>";
-print "$float<br>";
-print "$boolean<br>";
-print "$null<br>";
+$nuotrauka = 'src="../images/test.gif"';
+?>
+<html>
+    <head>
+        <title>Antra uzduotis</title>
+    </head>
+    <body>
+        <p><img <?php print $nuotrauka ?>></p>
+        <p><img <?php print $nuotrauka ?>></p>
+        <p><img <?php print $nuotrauka ?>></p>
+        <p><img <?php print $nuotrauka ?>></p>
+    </body>
+</html>
