e9df4482e877f7dfc7543125db9be9090484a7ee
donatasju <donatas.jurkusl@gmail.com>
2019-01-22 12:03:56 +0200

    for sure done

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
-$jievrai = (rand(1, 100));
-$antras_grazinti = (rand(201, 300));
-$pirmas_grazinti = (rand(101, 200));
+$jievrai = rand(1, 100);
+$pirmas_grazinti = rand(101, 200);
+$antras_grazinti = rand(201, 300);
 
 ?>
-
 <html>
     <head>
         <title>Skola</title>
     </head>
     <body>
-        <div>
-            <h1>Skolos skaiciuokle</h1>
-            <h2>Jei paemei <?php print $jievrai ?> jievru</h2>
-            <h3>Su dviem kabanciais grazinsi <?php print $antras_grazinti ?></h3>
-            <h4>Su vienu kabanciu grazinsi <?php print $pirmas_grazinti ?></h4>
-        </div>
+        <h1>Skolos skaiciuokle</h1>
+        <h2>Jei paemei <?php print $jievrai; ?> jievru</h2>
+        <p>
+            Su vienu kabanciu grazinsi <?php print $pirmas_grazinti; ?><br>
+            Su dviem kabanciais grazinsi <?php print $antras_grazinti; ?>
+        </p>
     </body>
 </html>
