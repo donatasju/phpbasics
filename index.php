31302fb4d1609a9272d40ea78d7f3fda6a9aa441
donatasju <donatas.jurkusl@gmail.com>
2019-01-22 12:27:23 +0200

    uzd 4

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
 <?php
-$jievrai = rand(1, 100);
-$pirmas_grazinti = rand(101, 200);
-$antras_grazinti = rand(201, 300);
-
+date_default_timezone_set('Europe/Vilnius');
+$siukslines_turis = 40;
+$dienos_siuksliu_turis = rand(10, 20);
+$max_kaupo_turis = rand (1, 20);
+$dienos = round(($siukslines_turis + $max_kaupo_turis) / $dienos_siuksliu_turis);
+$date = date('Y.m.d', strtotime("+$dienos days"));
 ?>
 <html>
     <head>
-        <title>Skola</title>
+        <title>Uzduotis 4</title>
     </head>
     <body>
-        <h1>Skolos skaiciuokle</h1>
-        <h2>Jei paemei <?php print $jievrai; ?> jievru</h2>
         <p>
-            Su vienu kabanciu grazinsi <?php print $pirmas_grazinti; ?><br>
-            Su dviem kabanciais grazinsi <?php print $antras_grazinti; ?>
+            Po <?php print "$dienos($date)" ?> dienu pirk geliu ir sampano, jeigu nori, kad zmona siuskles pati isinestu.
         </p>
     </body>
 </html>
