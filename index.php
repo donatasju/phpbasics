2a963c675bdc859f698e7fcf93d8c8c10cddf24e
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-22 11:54:16 +0200

    kodo taisymo uzduotis

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?php
-$nuotrauka = 'src="../images/test.gif"';
+$sudas1 = rand(1,100);
+$sudas2 = rand(101,200);
+$sudas3 = rand(201,300);
 ?>
 <html>
     <head>
-        <title>Antra uzduotis</title>
+        <title>Skolos skaiciuokle</title>
     </head>
     <body>
-        <p><img <?php print $nuotrauka ?>></p>
-        <p><img <?php print $nuotrauka ?>></p>
-        <p><img <?php print $nuotrauka ?>></p>
-        <p><img <?php print $nuotrauka ?>></p>
+        <h1>Skolos skaiciuokle</h1>
+        <p>
+            Jei paemei <?php print $sudas1; ?> jievru Su dviem kabanciais
+            grazinsi <?php print $sudas3; ?> Su vienu kabanciu grazinsi
+            <?php print $sudas2; ?>
+        </p>
     </body>
 </html>
+
