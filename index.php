b68b075ef98969f3c18835247315f085458bd5fb
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-22 11:17:42 +0200

    Antra diena pirma uzduotis tipai

--- index.php
+++ index.php
@@ -1,23 +1,12 @@
 <?php
-$sekunde = date("s");
-$sekunde_kart_10 = $sekunde * 10;
-?>
-<html>
-    <head>
-        <title>Bomba</title>
-        <link rel="stylesheet" href="css/main.css">
-        <style>
-            .klase-1 {
-                background-image: url("../images/bomb.jpg");
-                width: <?php print $sekunde_kart_10 ?>px;
-                height: <?php print $sekunde_kart_10 ?>px;
-                background-size: contain;
-                background-repeat: no-repeat;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="klase-1"></div>
-        <div><?php print $sekunde?></div>
-    </body>
-</html>
+$string = 'Labas';
+$integer = 123;
+$float = 1.5;
+$boolean = true;
+$null = null;
+
+print $string;
+print $integer;
+print $float;
+print $boolean;
+print $null;
