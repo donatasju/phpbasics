7e4d6b3e1ae3b99613f5ace725924260f72b1cdd
donatasju <donatas.jurkusl@gmail.com>
2019-01-22 11:50:23 +0200

    uzduotis #2

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
-$sudas1 = (rand(1,100));
-$sudas2 = (rand(101,200));
-$sudas3 = (rand(201,300));
+$sudas1 = (rand(1, 100));
+$sudas2 = (rand(101, 200));
+$sudas3 = (rand(201, 300));
 ?>
 
-<div>
-    <h1>Skolos skaiciuokle</h1>
-    <h2>Jei paemei <?php print $sudas1 ?> jievru</h2>
-    <h3>Su dviem kabanciais grazinsi <?php print $sudas3 ?></h3>
-    <h4>Su vienu kabanciu grazinsi <?php print $sudas2 ?></h4>
-</div>
+<html>
+    <head>
+        <title>Skola</title>
+    </head>
+    <body>
+        <div>
+            <h1>Skolos skaiciuokle</h1>
+            <h2>Jei paemei <?php print $sudas1 ?> jievru</h2>
+            <h3>Su dviem kabanciais grazinsi <?php print $sudas3 ?></h3>
+            <h4>Su vienu kabanciu grazinsi <?php print $sudas2 ?></h4>
+        </div>
+    </body>
+</html>
