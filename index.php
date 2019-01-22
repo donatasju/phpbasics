760156d72ae9383fa59e214989c72c856a2e2cdd
donatasju <donatas.jurkusl@gmail.com>
2019-01-22 12:33:18 +0200

    done4

--- index.php
+++ index.php
@@ -3,8 +3,9 @@ date_default_timezone_set('Europe/Vilnius');
 $siukslines_turis = 40;
 $dienos_siuksliu_turis = rand(10, 20);
 $max_kaupo_turis = rand (1, 20);
-$dienos = round(($siukslines_turis + $max_kaupo_turis) / $dienos_siuksliu_turis);
+$dienos = floor(($siukslines_turis + $max_kaupo_turis) / $dienos_siuksliu_turis);
 $date = date('Y.m.d', strtotime("+$dienos days"));
+
 ?>
 <html>
     <head>
