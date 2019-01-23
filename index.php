ad34c8c7511d9e3bf87eed9e7d0721e492962127
donatasju <donatas.jurkusl@gmail.com>
2019-01-23 11:09:55 +0200

    uzd2

--- index.php
+++ index.php
@@ -1,16 +1,19 @@
 <?php
-$t = rand(1, 10);
-$text = 'N';
-for ($i = 0; $i < $t; $i++){
-    $text.= 'x';
+$po_alaus = rand(1,5);
+$dar_alaus = 0;
+$kablys = 1;
+
+for ($i = 0; $i < $po_alaus; $i++){
+    $dar_alaus += floor(($po_alaus/2) + $kablys);
 }
 ?>
 <html>
     <head>
-        <title>Uzduotis 1</title>
+        <title>Uzduotis 2</title>
     </head>
     <body>
-        <h1>As po <?php print $t ?> valandu praktikos..</h1>
-        <h2><?php print $text ?> koks pavarges..</h2>
+        <p>
+            Po <?php print $po_alaus ;?> alaus, tiketina dar imsiu <?php print $dar_alaus; ?>
+        </p>
     </body>
 </html>
