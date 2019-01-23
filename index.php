694ffebdcf41c36457d5b06ae63cee41b9c5d0ef
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-23 11:13:16 +0200

    imsiu alaus uzduotis fix

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 $po_alaus = rand(1, 5);
 $dar_alaus = 0;
 $kablys = 1;
-for ($i = 0; $i < $po_alaus; $i++){
-    $dar_alaus += floor($po_alaus/2) + $kablys;
+for ($i = 0; $i < $po_alaus; $i++) {
+    $dar_alaus += floor($po_alaus / 2) + $kablys;
 }
 ?>
 <html>
@@ -11,6 +11,6 @@ for ($i = 0; $i < $po_alaus; $i++){
         <title>Alus</title>
     </head>
     <body>
-        <p>Po <?php print $po_alaus ?> alaus tiketina, dar imsiu <?php print $dar_alaus ?> alaus.</p>
+        <p>Po <?php print $po_alaus; ?> alaus tiketina, dar imsiu <?php print $dar_alaus; ?> alaus.</p>
     </body>
 </html>
