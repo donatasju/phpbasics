19bbcae5ff9b94677957f20f9f38172e2d97bd16
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-23 10:55:28 +0200

    pirmas nxxx uzduotis fix1

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ for ($i = 0; $i < $t; $i++) {
         <title>Nxx uzduotis</title>
     </head>
     <body>
-        <h1>As po <?php print $t ?> valandu praktikos</h1>
-        <h2><?php print $text ?> koks pavarges</h2>
+        <h1>As po <?php print $t; ?> valandu praktikos</h1>
+        <h2><?php print $text; ?> koks pavarges</h2>
     </body>
 </html>
