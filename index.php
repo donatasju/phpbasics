5b788204a5f1b6ff352d9fda0fd7840084da7143
donatasju <donatas.jurkusl@gmail.com>
2019-01-23 13:03:58 +0200

    uzd55

--- index.php
+++ index.php
@@ -3,13 +3,14 @@ $months = 12;
 $kisene = 1000;
 $alga = 700;
 
-
 for ($i = 0; $i <= $months; $i++) {
-    $islaidos = rand(50, 2000);
-    $rezult = ($kisene + $alga) - $islaidos;
-    if ($rezult < 0) {
-        print "Bloga prognoze: $i menesi gali baigtis pinigai! Atsargiai";
+    $islaidos = rand(50, 1700);
+    $kisene += $alga - $islaidos;
+    if ($kisene <= 0) {
+        $text = "Bloga prognoze: po $i menesi gali baigtis pinigai! Atsargiai. Likutis $kisene euru";
         break;
+    } else {
+        $text = "Tavo kiseneje $kisene euru po $i menesiu.";
     }
 }
 
@@ -20,7 +21,7 @@ for ($i = 0; $i <= $months; $i++) {
     </head>
     <body>
         <p>
-            Tavo kiseneje <?php print $rezult ?> euru.
+            <?php print $text; ?>
         </p>
     </body>
 </html>
