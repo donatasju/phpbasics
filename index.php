c09063fd38f7f6293d30c81bc67f032c0559d0ec
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-23 13:58:56 +0200

    Cigaretes uzduotis

--- index.php
+++ index.php
@@ -1,24 +1,33 @@
 <?php
-$months = 12;
-$kisene = 1000;
-$alga = 700;
+$days = 365;
+$pakelio_kaina = 3.50;
+$cigaretes_kaina = $pakelio_kaina / 20;
+$surukytos_cigaretes = 0;
+$ket_penk_cigaretes = 0;
 
-for ($i = 0; $i < $months; $i++) {
-    $islaidos = rand(500, 1000);
-    $kisene += $alga - $islaidos;
-    if ($kisene < 0) {
-        $babkes = "Babkes tau baigsis $i menesi";
-        break;
+for ($i = 0; $i < $days; $i++) {
+    $cizos_mon_thu = rand(3, 4);
+    $cizos_fri = rand(10, 20);
+    $cizos_sat_sun = rand(1, 3);
+    $savaites_diena = date('N', strtotime("+$i day"));
+    if ($savaites_diena >= 1 && $savaites_diena <= 4) {
+        $surukytos_cigaretes += $cizos_mon_thu;
+        $ket_penk_cigaretes += $cizos_mon_thu;
+    } elseif ($savaites_diena == 5) {
+        $surukytos_cigaretes += $cizos_fri;
     } else {
-        $babkes = "Tau liko: $kisene";
+        $surukytos_cigaretes += $cizos_sat_sun;
     }
 }
+$viso_kaina = $surukytos_cigaretes * $cigaretes_kaina;
+$ket_penk_cigareciu_kaina = $viso_kaina - $ket_penk_cigaretes * $cigaretes_kaina;
 ?>
 <html>
     <head>
-        <title>Alga</title>
+        <title>Cigaretes</title>
     </head>
     <body>
-        <?php print $babkes; ?>
+        <p>Per metus surukysiu <?php print $surukytos_cigaretes; ?> cigareciu uz <?php print $viso_kaina; ?></p>
+        <p>Nerukydamas Pirmadieni-ketvirtadieni sutaupyciau <?php print $ket_penk_cigareciu_kaina; ?> eur.</p>
     </body>
 </html>
