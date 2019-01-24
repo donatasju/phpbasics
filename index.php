dc5a7aa6c1328d91098b915fbea3cb6f32b443a5
donatasju <donatas.jurkusl@gmail.com>
2019-01-24 11:21:36 +0200

    uzd 88 ddoone

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 date_default_timezone_set('Europe/Vilnius');
 $days = 365;
-$rukau = 0;
-$nerukau = 0;
+$viso_cizu = 0;
+$nerukau_mon_thu = 0;
 $pakelio_kaina = 3.50;
 $cigaretes_kaina = $pakelio_kaina / 20;
 $rukymo_laikas = 5;
@@ -12,19 +12,19 @@ for ($i = 0; $i <= $days; $i++) {
     $date = date('N', strtotime("+$i day"));
     if ($date >= 1 && $date <= 4) {
         $cizos_mon_thu = rand(3, 10);
-        $nerukau += $cizos_mon_thu;
-        $rukau += $cizos_mon_thu;
+        $nerukau_mon_thu += $cizos_mon_thu;
+        $viso_cizu += $cizos_mon_thu;
     } elseif ($date == 5) {
         $cizos_fri = rand(10, 20);
-        $rukau += $cizos_fri;
+        $viso_cizu += $cizos_fri;
     } else {
         $cizos_sat_sun = rand(1, 5);
-        $rukau += $cizos_sat_sun;
+        $viso_cizu += $cizos_sat_sun;
     }
 }
-$viso_kaina = $rukau * $cigaretes_kaina;
-$nerukau = ($rukau - $nerukau) * $cigaretes_kaina;
-$viso_hours += $rukau * $rukymo_laikas / 60
+$viso_kaina = $viso_cizu * $cigaretes_kaina;
+$sutaupau = $nerukau_mon_thu * $cigaretes_kaina;
+$viso_hours += $viso_cizu * $rukymo_laikas / 60
 
 ?>
 <html>
@@ -33,12 +33,12 @@ $viso_hours += $rukau * $rukymo_laikas / 60
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-        <p>Per metus surukysiu <?php print $rukau; ?> cigareciu uz <?php print $viso_kaina; ?> euru.<br>
-            Nerukydamas Pirmad-Ketvirtadieni sutaupyciau <?php print $nerukau; ?> eur.<br>
+        <p>Per metus surukysiu <?php print $viso_cizu; ?> cigareciu uz <?php print $viso_kaina; ?> euru.<br>
+            Nerukydamas Pirmad-Ketvirtadieni sutaupyciau <?php print $sutaupau; ?> eur.<br>
             Per metus prastovesiu traukdamas <?php print $viso_hours ?> valandu.
         </p>
         <div class="flex">
-            <?php for ($j = 1; $j <= $rukau; $j++): ?>
+            <?php for ($j = 1; $j <= $viso_cizu; $j++): ?>
             <div class="cig"><?php print $j; ?></div>
             <?php endfor; ?>
         </div>
