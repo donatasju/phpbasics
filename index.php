c72e5c817185d6bde63523bc864d933eb107befd
donatasju <donatas.jurkusl@gmail.com>
2019-01-24 12:20:41 +0200

    uzd 9

--- index.php
+++ index.php
@@ -7,9 +7,10 @@ $pakelio_kaina = 3.50;
 $cigaretes_kaina = $pakelio_kaina / 20;
 $rukymo_laikas = 5;
 $viso_hours = 0;
-        
+
 for ($i = 0; $i <= $days; $i++) {
     $date = date('N', strtotime("+$i day"));
+
     if ($date >= 1 && $date <= 4) {
         $cizos_mon_thu = rand(3, 10);
         $nerukau_mon_thu += $cizos_mon_thu;
@@ -22,14 +23,16 @@ for ($i = 0; $i <= $days; $i++) {
         $viso_cizu += $cizos_sat_sun;
     }
 }
+
 $viso_kaina = $viso_cizu * $cigaretes_kaina;
 $sutaupau = $nerukau_mon_thu * $cigaretes_kaina;
-$viso_hours += $viso_cizu * $rukymo_laikas / 60
-
+$viso_hours += $viso_cizu * $rukymo_laikas / 60;
+$pakeliai = $viso_cizu / 20;
+$liko = $viso_cizu % 20;
 ?>
 <html>
     <head>
-        <title>Uzduotis 4</title>
+        <title>Uzduotis 8</title>
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
@@ -39,7 +42,16 @@ $viso_hours += $viso_cizu * $rukymo_laikas / 60
         </p>
         <div class="flex">
             <?php for ($j = 1; $j <= $viso_cizu; $j++): ?>
-            <div class="cig"><?php print $j; ?></div>
+                <?php if ($liko): ?>
+                    <div class="pakeliai">
+                        <?php for ($i = 1; $i <= 20; $i++): ?>
+                            <div class="cig"></div>
+                        <?php endfor; ?>
+                    </div>
+                <?php endif; ?>
+            <?php endfor; ?>
+            <?php for ($i = 1; $i <= $liko; $i++): ?>
+                <div class="cig"></div>
             <?php endfor; ?>
         </div>
     </body>
