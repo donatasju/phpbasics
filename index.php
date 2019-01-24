2df21148e3327683a727cc4962ab2d18a3a5b606
donatasju <donatas.jurkusl@gmail.com>
2019-01-24 12:53:31 +0200

    jojo

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-$vaisiai = ['Obuolys', 'Apelsinas', 'Cukinija'];
+
+$vaisiai = [
+    'du dideli' => 'kiausiniai',
+    'vienas mazas' => 'baklazanas',
+    'vienas didelis' => 'grietines indelis'];
 
 var_dump($vaisiai);
-
-?>
