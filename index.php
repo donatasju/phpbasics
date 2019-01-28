db15057c66990fbaf5f1ea4297314a875b52422a
donatasju <donatas.jurkusl@gmail.com>
2019-01-28 13:43:00 +0200

    dishes

--- index.php
+++ index.php
@@ -1,65 +1,40 @@
 <?php
-$mano_atmintis = [
-    'Penktadienis',
-    'Alaus',
-    'Degtine',
-    'GinTonic',
-    'Viskis',
-    'Parukyt',
-    'degtine',
-    'kokteilis',
-    'parukyt',
-    'bbz'
+$dishes = [
+    [
+        'name' => 'Nut salad',
+        'price' => 3.44,
+        'ingredients' => [
+            'Nuts',
+            'Joghurt'
+        ]
+    ],
+    [
+        'name' => 'Bulldish',
+        'price' => 4.77,
+        'ingredients' => [
+            'Rice',
+            'Soja Sauce'
+        ]
+    ]
 ];
-$draugo_atmintis = [
-    'Penktadienis',
-    'Alus',
-    'Degtine',
-    'GinTonic',
-    'Viskis',
-    'Neruko',
-    'Tulikas',
-    'Vemt',
-    'neruko',
-    'koma'
-];
-
-$counter = count($mano_atmintis);
-$rand_flashback = $mano_atmintis[rand(0, $counter - 1)];
-
-foreach ($mano_atmintis as $value) {
-    if (in_array($value, $draugo_atmintis)) {
-        $bendra_atmintis [] = $value;
-    }
-};
-if (!in_array($rand_flashback, $bendra_atmintis)) {
-    $bendra_atmintis [] = $rand_flashback;
-}
 ?>
+
 <html>
     <head>
-        <title>uzd 2</title>
+        <title>dishes</title>
     </head>
     <body>
-        <h1>WTF?!</h1>
-        <h2>Mano atmintis:</h2>
-        <ul>
-            <?php foreach ($mano_atmintis as $value): ?>
-                <li><?php print $value; ?></li>
+        <div class="dishes">
+            <?php foreach ($dishes as $value): ?>
+                <h2><?php print "{$value['name']} - {$value['price']}"; ?></h2>
+                <ol>
+                    <?php foreach ($value['ingredients'] as $ingredient): ?>
+                        <li>
+                            <?php print $ingredient; ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ol>
             <?php endforeach; ?>
-        </ul>
-        <h3>Flashback: <?php print $rand_flashback; ?></h3>
-        <h2>Draugo atmintis:</h2>
-        <ul>
-            <?php foreach ($draugo_atmintis as $value): ?>
-                <li><?php print $value; ?></li>
-            <?php endforeach; ?>
-        </ul>
-        <h2>Bendra atmintis:</h2>
-        <ul>
-            <?php foreach ($bendra_atmintis as $value): ?>
-                <li><?php print $value; ?></li>
-            <?php endforeach; ?>
-        </ul>
+        </div>
     </body>
 </html>
