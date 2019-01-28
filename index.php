5042273fd180b09d7e835ef2144c4e3f2c7058c1
donatasju <donatas.jurkusl@gmail.com>
2019-01-28 12:32:18 +0200

    draugo atmintis added

--- index.php
+++ index.php
@@ -11,6 +11,18 @@ $mano_atmintis = [
     'parukyt',
     'bbz'
 ];
+$draugo_atmintis = [
+    'Penktadienis',
+    'Alus',
+    'Degtine',
+    'GinTonic',
+    'Viskis',
+    'Neruko',
+    'Tulikas',
+    'Vemt',
+    'neruko',
+    'koma'
+];
 $counter = count($mano_atmintis);
 $rand_flashback = $mano_atmintis[rand(0, $counter -1)];
 ?>
@@ -21,6 +33,17 @@ $rand_flashback = $mano_atmintis[rand(0, $counter -1)];
     <body>
         <h1>WTF?!</h1>
         <h2>Mano atmintis:</h2>
+        <ul>
+            <?php foreach ($mano_atmintis as $value): ?>
+            <li><?php print $value; ?></li>
+            <?php endforeach; ?>
+        </ul>
         <h3>Flashback: <?php print $rand_flashback; ?></h3>
+        <h2>Draugo atmintis:</h2>
+        <ul>
+            <?php foreach ($draugo_atmintis as $value): ?>
+            <li><?php print $value; ?></li>
+            <?php endforeach; ?>
+        </ul>
     </body>
 </html>
