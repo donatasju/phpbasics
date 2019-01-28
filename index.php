4f1ef8807da0ebdac4e67b72e4b8323ccde62a95
donatasju <donatas.jurkusl@gmail.com>
2019-01-28 12:33:08 +0200

    draugo atmintis added1

--- index.php
+++ index.php
@@ -35,14 +35,14 @@ $rand_flashback = $mano_atmintis[rand(0, $counter -1)];
         <h2>Mano atmintis:</h2>
         <ul>
             <?php foreach ($mano_atmintis as $value): ?>
-            <li><?php print $value; ?></li>
+                <li><?php print $value; ?></li>
             <?php endforeach; ?>
         </ul>
         <h3>Flashback: <?php print $rand_flashback; ?></h3>
         <h2>Draugo atmintis:</h2>
         <ul>
             <?php foreach ($draugo_atmintis as $value): ?>
-            <li><?php print $value; ?></li>
+                <li><?php print $value; ?></li>
             <?php endforeach; ?>
         </ul>
     </body>
