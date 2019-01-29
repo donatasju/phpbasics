f6d98f84683faae38ec7c8e8463fc532a72787f7
donatasju <donatas.jurkusl@gmail.com>
2019-01-29 11:49:58 +0200

    rankinukas done

--- index.php
+++ index.php
@@ -1,40 +1,42 @@
 <?php
-$dishes = [
-    [
-        'name' => 'Nut salad',
-        'price' => 3.44,
-        'ingredients' => [
-            'Nuts',
-            'Joghurt'
-        ]
-    ],
-    [
-        'name' => 'Bulldish',
-        'price' => 4.77,
-        'ingredients' => [
-            'Rice',
-            'Soja Sauce'
-        ]
-    ]
+$daiktai = [
+    'Kremas',
+    'Riesutai',
+    'Telefonas',
+    'Kompas',
+    'Masina',
+    'Klavetura',
+    'fakin hell',
+    'dahuje dalyku'
 ];
+$rankinuko_size = rand(1, 10);
+
+for ($i = 0; $i <= $rankinuko_size; $i++) {
+    $is_dark = rand(0, 1);
+    if ($is_dark) {
+        $text = 'Dark';
+    } else {
+        $text = 'Light';
+    }
+    $size = rand(2, 30);
+    $daiktai_size = rand(0, count($daiktai) - 1);
+    $random_pavadinimas = $daiktai[$daiktai_size];
+    $rankinukas[] = [
+        'name' => $random_pavadinimas,
+        'size' => $size,
+        'is_dark' => $text,
+        'info' => "$random_pavadinimas uzima $size cm3. Daiktas yra $text"
+    ];
+}
 ?>
 
 <html>
     <head>
-        <title>dishes</title>
+        <title></title>
     </head>
     <body>
-        <div class="dishes">
-            <?php foreach ($dishes as $value): ?>
-                <h2><?php print "{$value['name']} - {$value['price']}$"; ?></h2>
-                <ol>
-                    <?php foreach ($value['ingredients'] as $ingredient): ?>
-                        <li>
-                            <?php print $ingredient; ?>
-                        </li>
-                    <?php endforeach; ?>
-                </ol>
-            <?php endforeach; ?>
-        </div>
+        <?php foreach ($rankinukas as $daiktas): ?>
+            <p><?php print $daiktas['info']; ?></p>
+        <?php endforeach; ?>
     </body>
 </html>
