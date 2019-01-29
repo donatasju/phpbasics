9fd91e93ba1c00af293c71e0644e01ae01c5763e
donatasju <donatas.jurkusl@gmail.com>
2019-01-29 13:38:49 +0200

    first function

--- index.php
+++ index.php
@@ -1,53 +1,18 @@
 <?php
-$daiktai = [
-    'Kremas',
-    'Riesutai',
-    'Telefonas',
-    'Kompas',
-    'Masina',
-    'Klavetura',
-    'fakin hell',
-    'dahuje dalyku'
-];
-$rankinuko_size = rand(1, 10);
-$all_size = 0;
 
-for ($i = 0; $i <= $rankinuko_size; $i++) {
-    $is_dark = rand(0, 1);
-    $size = rand(2, 30);
-    $all_size += $size;
-    if ($is_dark) {
-        $text = 'Dark';
-    } else {
-        $text = 'Light';
-    }
-    $daiktai_size = rand(0, count($daiktai) - 1);
-    $random_pavadinimas = $daiktai[$daiktai_size];
-    $rankinukas[] = [
-        'name' => $random_pavadinimas,
-        'size' => $size,
-        'is_dark' => $text,
-        'info' => "$random_pavadinimas uzima $size cm3. Daiktas yra $text. Tikimybe:",
-    ];
-    $all_size += $size;
+function add($x, $y) {
+    $sum = $x + $y;
+    print $sum;
 }
-foreach($rankinukas as &$value) {
-    $daikto_tikimybe = round($value['size'] / $all_size * 100);
-    if ($value['is_dark']) {
-        $daikto_tikimybe /= 2;
-    }
-    $value['tikimybe'] = $daikto_tikimybe;
-    $value['info'] .= $daikto_tikimybe . '%';
-}
+
+$suma = add(5, 4)
 ?>
-
 <html>
     <head>
         <title></title>
+
     </head>
     <body>
-        <?php foreach ($rankinukas as $daiktas): ?>
-            <p><?php print $daiktas['info'] ?></p>
-        <?php endforeach; ?>
+        <h1><?php print $suma; ?> </h1>
     </body>
 </html>
