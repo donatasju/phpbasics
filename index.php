f522f8465d787e4ace5e693f5f6d5e2f4f320bc6
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-29 13:42:47 +0200

    function 1

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
-function sum($x, $y){
-    $suma = $x +$y;
-    print $tekstas = "$x + $y suma: $suma";
-}
 
+function sum($x, $y) {
+    $suma = $x + $y;
+    print "$x + $y suma: $suma";
+}
 ?>
 <html>
     <head>
         <title>Funkcijos</title>
     </head>
     <body>
-        <h1><?php sum(9, 5);?> </h1>
+        <h1><?php sum(9, 5); ?> </h1>
     </body>
 </html>
