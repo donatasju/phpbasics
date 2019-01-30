d55326949da50a7bcc3f2dac965a1cec455c99c3
donatasju <donatas.jurkusl@gmail.com>
2019-01-30 11:55:16 +0200

    table array

--- index.php
+++ index.php
@@ -3,21 +3,42 @@
 $balius = [
     [
         'daiktas' => 'Zuvis',
-        'name' => 'Laura',
+        'owner' => 'Laura',
         'kiekis' => 1,
         'matas' => 'kibiras'
     ],
     [
         'daiktas' => 'Baklazanai',
-        'name' => 'Tomas',
+        'owner' => 'Tomas',
         'kiekis' => 18,
         'matas' => 'kg'
     ],
     [
         'daiktas' => 'Aliejus',
-        'name' => 'Monika',
+        'owner' => 'Monika',
         'kiekis' => 0.7,
-        'matas' => 'litro'
+        'matas' => 'litras'
     ]
 ];
-var_dump($balius);
+
+?>
+
+<html>
+    <head>
+        <title>Balius</title>
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <?php foreach ($balius as $value): ?>
+            <table>
+                <tr>
+                    <td><?php print $value['kiekis']; ?></td>
+                    <td class="<?php print $value['matas']; ?>"></td>
+                    <td>Daiktas - <?php print $value['daiktas']; ?>;</td>
+                    <td>Owner - <?php print $value['owner']; ?></td>
+                </tr>
+            </table>
+        <?php endforeach; ?>
+        
+    </body>
+</html>
