37cd09b8b3c6811cc28be500f7890ab3be83b4ae
donatasju <donatas.jurkusl@gmail.com>
2019-01-30 12:51:59 +0200

    poeazija done

--- index.php
+++ index.php
@@ -1,41 +1,17 @@
 <?php
+$words = ['ne', 'nebegeriu', 'rukau', 'nerukau', 'geriu', 'telefonas', 'ekranas', 'programming', 'php', 'netbeans'];
+$text = '';
 
-$masina = [
-    [
-        'gamintojas' => 'fiat',
-        'modelis' => '126p',
-        'metai' => [
-            'from' => 1972,
-            'to' => 2000
-        ],
-        'gamyklos' => [
-            'Kasinas',
-            'Termini Imereze',
-            'Kragujevacas',
-            'Belsko-Biala',
-            'Tychos'
-        ],
-        'klase' => 'Miesto automobilis',
-        'kebulo_tipas' => [
-            '2 duru sedanas',
-            '2 duru hecbekas'
-        ],
-        'varikliai' => [
-            [
-                'turis_cm3' => 594,
-                'cilindrai' => 2
-            ],
-            [
-                'turis_cm3' => 594,
-                'cilindrai' => 2
-            ],
-            [
-                'turis_cm3' => 594,
-                'cilindrai' => 2
-            ]
-        ],
-        'dizaineris' => 'Sergio Sartorelli'
-    ]
-];
-var_dump($masina);
-
+for ($i = 0; $i < 300; $i++) {
+    $text .= ' ' . $words[rand(0, count($words) - 1)];
+    $i = strlen($text);
+}
+?>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <p><?php print "$i $text"; ?></p>
+    </body>
+</html>
