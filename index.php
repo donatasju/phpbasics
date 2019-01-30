953daefce43e4d1fa7c123f74464a4cd5a980de5
donatasju <donatas.jurkusl@gmail.com>
2019-01-30 11:07:40 +0200

    baliaus array1

--- index.php
+++ index.php
@@ -2,25 +2,22 @@
 
 $balius = [
     [
-        'zuvis' => [
-            'name' => 'Laura',
-            'kiekis' => 1,
-            'matas' => 'kibiras'
-        ]
+        'daiktas' => 'Zuvis',
+        'name' => 'Laura',
+        'kiekis' => 1,
+        'matas' => 'kibiras'
     ],
     [
-        'baklazanai' => [
-            'name' => 'Tomas',
-            'kiekis' => 18,
-            'matas' => 'kg'
-        ]
+        'daiktas' => 'Baklazanai',
+        'name' => 'Tomas',
+        'kiekis' => 18,
+        'matas' => 'kg'
     ],
     [
-        'Aliejus' => [
-            'name' => 'Monika',
-            'kiekis' => 0.7,
-            'matas' => 'litro'
-        ]
+        'daiktas' => 'Aliejus',
+        'name' => 'Monika',
+        'kiekis' => 0.7,
+        'matas' => 'litro'
     ]
 ];
 var_dump($balius);
