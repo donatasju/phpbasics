9c1cda6f826813111f9348aac017e8b6c56ce900
donatasju <donatas.jurkusl@gmail.com>
2019-01-30 11:00:00 +0200

    baliaus array

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
 <?php
 
-function add($x, $y) {
-    $sum = $x + $y;
-    return $sum;
-}
-?>
-<html>
-    <head>
-        <title></title>
-
-    </head>
-    <body>
-        <h1>Suma: <?php print add(5, 4); ?> </h1>
-    </body>
-</html>
+$balius = [
+    [
+        'zuvis' => [
+            'name' => 'Laura',
+            'kiekis' => 1,
+            'matas' => 'kibiras'
+        ]
+    ],
+    [
+        'baklazanai' => [
+            'name' => 'Tomas',
+            'kiekis' => 18,
+            'matas' => 'kg'
+        ]
+    ],
+    [
+        'Aliejus' => [
+            'name' => 'Monika',
+            'kiekis' => 0.7,
+            'matas' => 'litro'
+        ]
+    ]
+];
+var_dump($balius);
