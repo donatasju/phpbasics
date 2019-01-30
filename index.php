97221f760523b5eb6882648f63d3f0b40892ca83
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-30 11:30:18 +0200

    treniruojames multi-array

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $balius = [
         'daiktas' => 'Zuvis',
         'kiekis' => 1,
         'matas' => 'kibiras',
-        'owneris' => 'Laura'        
+        'owneris' => 'Laura'
     ],
     [
         'daiktas' => 'Baklazanai',
@@ -13,9 +13,28 @@ $balius = [
         'matas' => 'kg',
         'owneris' => 'Tomas'
     ],
+    [
         'daiktas' => 'Aliejus',
         'kiekis' => 0.7,
         'matas' => 'litras',
         'owneris' => 'Monika'
+    ]
 ];
 var_dump($balius);
+?>
+<html>
+    <head>
+        <title>Balius masyvas</title>
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <?php foreach($balius as $value) :?>
+        <div class="inventorius">
+            <p>Kiekis: <?php print $value['kiekis']; ?></p>
+            <div class="<?php print $value['matas']?>"></div>
+            <p>Daiktas: <?php print $value['daiktas']; ?></p>
+            <p>Kas atsinese: <?php print $value['owneris']; ?></p>
+        </div>
+        <?php endforeach; ?>
+    </body>
+</html>
