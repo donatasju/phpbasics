ef3a7983154af09847cd36011fb3353a3d37b10c
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-31 10:52:27 +0200

    random slot mashine fix

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
 function slot_run() {
+    $array = [];
     for ($i = 0; $i < 3; $i++) {
-        $random_skaicius = rand(0, 1);
-        $array[] = $random_skaicius;
+        for ($j = 0; $j < 3; $j++) {
+            $array[$i][$j] = rand(0, 1);
+        }
     }
-    for ($i = 0; $i < 3; $i++) {
-        $array_3_skaiciu[] = $array;
-    }
-    return $array_3_skaiciu;
+    return $array;
 }
 
 var_dump(slot_run());
+?>
