5bc8ba82f81242dd62a88a95ddfa6687d147c496
Katamaranas <ernestas.zidokas@gmail.com>
2019-01-31 11:41:58 +0200

    random rubic kubic

--- index.php
+++ index.php
@@ -1,14 +1,29 @@
 <?php
 
-function slot_run() {
+function slot_run($rows, $cols) {
     $array = [];
-    for ($i = 0; $i < 3; $i++) {
-        for ($j = 0; $j < 3; $j++) {
+    for ($i = 0; $i < $rows; $i++) {
+        for ($j = 0; $j < $cols; $j++) {
             $array[$i][$j] = rand(0, 1);
         }
     }
     return $array;
 }
 
-var_dump(slot_run());
+$slot_mashine = slot_run(3, 3);
 ?>
+<html>
+    <head>
+        <title>Slot mashine</title>
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <?php foreach ($slot_mashine as $row): ?>
+            <div class="kubas">
+                <?php foreach ($row as $skaicius): ?>
+                    <div class="stulpelis klase_<?php print $skaicius ?>"></div>
+                <?php endforeach; ?>
+            </div>
+        <?php endforeach; ?>
+    </body>
+</html>
