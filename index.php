d91896ae99b5b771fff49e488816e81e382a0618
donatasju <donatas.jurkusl@gmail.com>
2019-02-04 11:46:17 +0200

    array function 2

--- index.php
+++ index.php
@@ -23,11 +23,13 @@ $bbd = [
 ];
 
 function pzdamat($bbd) {
-    foreach($bbd as &$daiktas) {
+    foreach ($bbd as &$daiktas) {
         $daiktas['color'] = 'red';
     }
+    
     return $bbd;
 }
+
 $bbd = pzdamat($bbd);
 ?>
 
@@ -38,11 +40,11 @@ $bbd = pzdamat($bbd);
     </head>
     <body>
         <div class="flex">
-            <?php foreach ($bbd as $daiktas): ?>
+<?php foreach ($bbd as $daiktas): ?>
                 <div class="block <?php print $daiktas['color'] . ' ' . $daiktas['form']; ?>">
                     <span><?php print $daiktas['tekstas']; ?></span>
                 </div>
-            <?php endforeach; ?>
+<?php endforeach; ?>
         </div>
     </body>
 </html>
