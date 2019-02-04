93d58270163cdd2224f516ca6c8f538a868b77a6
Katamaranas <ernestas.zidokas@gmail.com>
2019-02-04 10:24:14 +0200

    bbd mat array

--- index.php
+++ index.php
@@ -1,42 +1,26 @@
 <?php
 
-function slot_run($rows, $cols) {
-    $array = [];
-    for ($i = 0; $i < $rows; $i++) {
-        for ($j = 0; $j < $cols; $j++) {
-            $array[$i][$j] = rand(0, 1);
-        }
-    }
-    return $array;
-}
-
-function slot_check($array) {
-    $winners = [];
-    foreach ($array as $key => $value) {
-        if (array_sum($value) == count($value)) {
-            $winners[] = $key;
-        }
-    }
-    return $winners;
-}
-
-$game = slot_run(3, 3);
-$laimetojai = slot_check($game);
+$bbd = [
+    [
+        'forma' => 'apvali',
+        'spalva' => 'zalia',
+        'textas' => 'AS'
+    ],
+    [
+        'forma' => 'kvadratas',
+        'spalva' => 'zalia',
+        'textas' => 'B'
+    ],
+    [
+        'forma' => 'kvadratas',
+        'spalva' => 'orange',
+        'textas' => 'B'
+    ],
+    [
+        'forma' => 'kvadratas',
+        'spalva' => 'raudona',
+        'textas' => 'D'
+    ]
+];
+var_dump($bbd);
 ?>
-<html>
-    <head>
-        <title>Slot mashine</title>
-        <link rel="stylesheet" href="css/main.css">
-    </head>
-    <body>
-        <div class="container">
-            <?php foreach ($game as $key => $row): ?>
-                <div class="kubas-<?php print (in_array($key, $laimetojai) ? 'win' : 'loose'); ?>">
-                    <?php foreach ($row as $skaicius): ?>
-                        <div class="stulpelis klase_<?php print $skaicius ?>"></div>
-                    <?php endforeach; ?>
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </body>
-</html>
