49235c5856024f06c922b707009d8e02a0cbdc42
Katamaranas <ernestas.zidokas@gmail.com>
2019-02-06 13:33:29 +0200

    formos didinti mygtuko skaiciu +1 + nuotraukos

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 $skaicius = 0;
 
-if(isset($_POST['mygtukas'])){
-    $skaicius = $_POST['mygtukas'] +1;
+if (isset($_POST['mygtukas'])) {
+    $skaicius = $_POST['mygtukas'] + 1;
 }
 ?>
 <html>
@@ -12,6 +12,9 @@ if(isset($_POST['mygtukas'])){
     <body>
         <form action="index.php" method="POST">
             <button name="mygtukas" value="<?php print $skaicius ?>">Paspaudimo Nr. <?php print $skaicius ?></button>
+            <?php for ($i = 0; $i < $skaicius; $i++): ?>
+                <img src="images/test.gif" height="200" width="200">
+            <?php endfor; ?> 
         </form>
     </body>
 </html>
