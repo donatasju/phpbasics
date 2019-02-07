c2e5d6fabfdcaeddbd675077dfe9e801ece4dbdb
Katamaranas <ernestas.zidokas@gmail.com>
2019-02-07 12:50:32 +0200

    ikelti nuotrauka forma + masyvas

--- index.php
+++ index.php
@@ -1,17 +1,54 @@
 <?php
 $file = $_FILES['file'] ?? false;
-$dir = 'uploads';
 
-function save_file($file, $dir) {
-    if ($file['error'] == 0) {
-        $target_file_name = time() . '-' . $file['name'];
+$masyvas = [
+    [
+        'klausimas' => 'Ar sasalini sasyska?',
+        [
+            'atsakymas' => ['taip', 'Xujnia', 'nebepisk proto']
+        ],
+        'teisingas' => 2
+    ],
+    [
+        'klausimas' => 'Ar bybis kiausai?',
+        [
+            'atsakymas' => ['zopa', 'pizdiec', 'taskau ir lapnoju']
+        ],
+        'teisingas' => 0
+    ],
+    [
+        'klausimas' => 'Ar myli burokus?',
+        [
+            'atsakymas' => ['taip', 'visiskai', 'sikna']
+        ],
+        'teisingas' => 1
+    ],
+    [
+        'klausimas' => 'Ar kartais atsimeki?',
+        [
+            'atsakymas' => ['saules akiniai = cool guy', 'ne', 'bijau nes pamatysiu Donata']
+        ],
+        'teisingas' => 2
+    ],
+    [
+        'klausimas' => 'Ar pizda vapse?',
+        [
+            'atsakymas' => ['neviltis...', 'soksiu pro langa be parasiuto', 'laukiu penktadienio ir alaus']
+        ],
+        'teisingas' => 2
+    ]
+];
+
+function save_file($file, $dir = 'uploads', $allowed_types = ['image/jpeg', 'image/png']) {
+    if ($file['error'] == 0 && in_array($file['type'], $allowed_types)) {
+        $target_file_name = microtime() . '-' . strtolower($file['name']);
         $target_path = $dir . '/' . $target_file_name;
 
         if (move_uploaded_file($file['tmp_name'], $target_path)) {
             return true;
         }
-        return false;
     }
+    return false;
 }
 
 if (!empty($file)) {
