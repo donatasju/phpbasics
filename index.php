3364331cea044fb78b257435d9874aef6244e230
Katamaranas <ernestas.zidokas@gmail.com>
2019-02-07 10:18:07 +0200

    formos Ducho anketa full

--- index.php
+++ index.php
@@ -8,12 +8,17 @@ $lygis = $_POST['lygis'] ?? false;
 
 if (!empty($vardas && $pavarde && $lygis)) {
     $sertifikato_array = [
-        'tipas' => 'DUCHO SERTIFIKATAS',
-        'vardas' => $vardas,
-        'pavarde' => $pavarde,
-        'amzius' => $amzius,
-        'lygis' => $lygis
+        'antraste' => 'DUCHO SERTIFIKATAS',
+        'duomenys' => [
+            'vardas' => $vardas,
+            'pavarde' => $pavarde,
+            'amzius' => $amzius,
+            'lygis' => $lygis
+        ]
     ];
+    if (empty($amzius)) {
+        unset($sertifikato_array['duomenys']['amzius']);
+    }
 }
 ?>
 <html>
@@ -23,25 +28,28 @@ if (!empty($vardas && $pavarde && $lygis)) {
     </head>
     <body>
         <form action="index.php" method="POST">
-            <p>Vardas: <input name="vardas" type="text" placeholder="pvz. Jonas" required/></p>
-            <p>Pavarde: <input name="pavarde" type="text" placeholder="pvz. Petrauskas" required/></p>
-            <p>Amzius: <input name="amzius" type="number" placeholder="pvz. 26"/></p>
-            <p>
+            <label>Vardas: <input name="vardas" type="text" placeholder="pvz. Jonas" required/></label>
+            <label>Pavarde: <input name="pavarde" type="text" placeholder="pvz. Petrauskas" required/></label>
+            <label>Amzius: <input name="amzius" type="number" placeholder="pvz. 26"/></label>
+            <label>
                 Lygis: 
                 <select name="lygis">
                     <option value="pradedantysis" selected>Pradedantysis</option>
                     <option value="pazenges">Pazenges</option>
                     <option value="profas">Profas</option>
                 </select>
-            </p>
+            </label>
             <button type="submit">Generuoti Ducho Sertifikata</button>
         </form>
-        <div class="<?php !empty($sertifikato_array) ? print 'border' : ''; ?>">            
-            <?php foreach ($sertifikato_array as $key => $value): ?>
-                <span class="blokas">
-                    <?php $value == $sertifikato_array['tipas'] ? print $value : print $key . ': ' . $value; ?>
-                </span>
-            <?php endforeach; ?>
-        </div> 
+        <?php if (!empty($sertifikato_array)): ?>
+            <div class="border">      
+                <h2><?php print $sertifikato_array['antraste']; ?></h2>
+                <?php foreach ($sertifikato_array['duomenys'] as $key => $value): ?>                    
+                    <span class="blokas">
+                        <?php print $key . ': ' . $value; ?>
+                    </span>
+                <?php endforeach; ?>
+            </div> 
+        <?php endif; ?>
     </body>
 </html>
