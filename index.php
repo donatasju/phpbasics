74c487db140993e8661d976dcc43e2a4a03f58f5
donatasju <donatas.jurkusl@gmail.com>
2019-02-07 12:27:07 +0200

    dainiaus upload

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <?php
-$dir = 'uploaded/photos';
+$dir = 'uploaded';
 $file = $_FILES['photo'] ?? false;
-var_dump($file);
-function save_file($file, $dir) {
-    if ($file['error'] == 0) {
-        $target_fname = time() . ' ' . $file['name'];
-        $target_path = $dir . '/' . $target_fname;
+
+/**
+ * Saves file from a superglobal $_FILES
+ * 
+ * @param array $file $_FILES['file_index']
+ * @param string $dir
+ * @param array $allowed_types Allowed file mime types. Ex.: 'image/jpeg'
+ * @return boolean false if error
+ */
+function save_file($file, $dir = 'uploads', $allowed_types = ['image/jpeg', 'image/png']) {
+    if ($file['error'] == 0 && in_array($file['type'], $allowed_types)) {
+        $target_file_name = microtime() . '-' . strtolower($file['name']);
+        $target_path = $dir . '/' . $target_file_name;
+
         if (move_uploaded_file($file['tmp_name'], $target_path)) {
-            
             return true;
         }
     }
