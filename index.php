57420c8bd2b4fa252efefe49883bf2f571e53174
Katamaranas <ernestas.zidokas@gmail.com>
2019-02-07 11:40:33 +0200

    ikelti nuotrauka forma

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
-$sertifikato_array = [];
+$file = $_FILES['file'] ?? false;
+$dir = 'uploads';
 
-$vardas = $_POST['vardas'] ?? false;
-$pavarde = $_POST['pavarde'] ?? false;
-$amzius = $_POST['amzius'] ?? false;
-$lygis = $_POST['lygis'] ?? false;
-
-if (!empty($vardas && $pavarde && $lygis)) {
-    $sertifikato_array = [
-        'antraste' => 'DUCHO SERTIFIKATAS',
-        'duomenys' => [
-            'vardas' => $vardas,
-            'pavarde' => $pavarde,
-            'amzius' => $amzius,
-            'lygis' => $lygis
-        ]
-    ];
-    if (empty($amzius)) {
-        unset($sertifikato_array['duomenys']['amzius']);
+function save_file($file, $dir) {    
+    if ($file['error'] == 0) {
+        $target_file_name = time() . $file['name'];
+        $target_path = $dir . '/' . $target_file_name;
+        
+        if (move_uploaded_file($file['tmp_name'], $target_path)) {
+            $success = true;
+        } else {
+            $success = false;
+        }
+    } else {
+        $errors = true;
     }
+}
+
+if (!empty($file)) {
+    save_file($file, $dir);
 }
 ?>
 <html>
@@ -27,29 +27,9 @@ if (!empty($vardas && $pavarde && $lygis)) {
         <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
-        <form action="index.php" method="POST">
-            <label>Vardas: <input name="vardas" type="text" placeholder="pvz. Jonas" required/></label>
-            <label>Pavarde: <input name="pavarde" type="text" placeholder="pvz. Petrauskas" required/></label>
-            <label>Amzius: <input name="amzius" type="number" placeholder="pvz. 26"/></label>
-            <label>
-                Lygis: 
-                <select name="lygis">
-                    <option value="pradedantysis" selected>Pradedantysis</option>
-                    <option value="pazenges">Pazenges</option>
-                    <option value="profas">Profas</option>
-                </select>
-            </label>
-            <button type="submit">Generuoti Ducho Sertifikata</button>
+        <form enctype="multipart/form-data" method="POST">
+            Tavo foto:<input name="file" type="file">
+            <input type="submit" value="upload">
         </form>
-        <?php if (!empty($sertifikato_array)): ?>
-            <div class="border">      
-                <h2><?php print $sertifikato_array['antraste']; ?></h2>
-                <?php foreach ($sertifikato_array['duomenys'] as $key => $value): ?>                    
-                    <span class="blokas">
-                        <?php print $key . ': ' . $value; ?>
-                    </span>
-                <?php endforeach; ?>
-            </div> 
-        <?php endif; ?>
     </body>
 </html>
