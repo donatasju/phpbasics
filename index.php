9cfc39c8c9d132486b5125c0da71d03b51727a95
donatasju <donatas.jurkusl@gmail.com>
2019-02-11 11:11:28 +0200

    cant hack anymore

--- index.php
+++ index.php
@@ -1,99 +1,19 @@
 <?php
-$dir = 'uploaded';
-$file = $_FILES['photo'] ?? false;
-
-$klausimynas = [
-    [
-        'klausimas' => [
-            'kas duhas?'
-        ],
-        'atsakymai' => [
-            'bybis',
-            'kiausai',
-            'vista'
-        ],
-        'teisingas' => 2
-    ],
-    [
-        'klausimas' => [
-            'hujova masina'
-        ],
-        'atsakymai' => [
-            'Silke',
-            'Betmenas',
-            'Zopelis'
-        ],
-        'teisingas' => 2
-    ],
-    [
-        'klausimas' => [
-            'Whos the best'
-        ],
-        'atsakymai' => [
-            'PHP',
-            'JAVA',
-            'Nieko'
-        ],
-        'teisingas' => 0
-    ],
-    [
-        'klausimas' => [
-            'Lauki penktadienio?'
-        ],
-        'atsakymai' => [
-            'Nihuje',
-            'Alaus',
-            'Jop'
-        ],
-        'teisingas' => 1
-    ],
-    [
-        'klausimas' => [
-            'Nieko gero?'
-        ],
-        'atsakymai' => [
-            'gero',
-            'aha',
-            'nieko'
-        ],
-        'teisingas' => 1
-    ]
-];
-var_dump($klausimynas);
-/**
- * Saves file from a superglobal $_FILES
- * 
- * @param array $file $_FILES['file_index']
- * @param string $dir
- * @param array $allowed_types Allowed file mime types. Ex.: 'image/jpeg'
- * @return boolean false if error
- */
-function save_file($file, $dir = 'uploads', $allowed_types = ['image/jpeg', 'image/png']) {
-    if ($file['error'] == 0 && in_array($file['type'], $allowed_types)) {
-        $target_file_name = microtime() . '-' . strtolower($file['name']);
-        $target_path = $dir . '/' . $target_file_name;
-
-        if (move_uploaded_file($file['tmp_name'], $target_path)) {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-if (!empty($_FILES)) {
-    save_file($file, $dir);
-}
+$input =  filter_input_array(INPUT_POST, [
+    'vardas' => FILTER_SANITIZE_SPECIAL_CHARS,
+        ]);
 ?>
-
 <html>
     <head>
-        <title>Files</title>
+        <title>Hack Me</title>
+        <link rel="stylesheet" href="css/style.css"/>
     </head>
     <body>
-        <form enctype="multipart/form-data" method="POST">
-            Tavo foto: <input name="photo" type="file">
-            <input type="submit" value="Gauti rezultata">
+        <h1><?php print $input['vardas'] ?? ''; ?></h1>
+        <h2>Hack this page</h2>
+        <form method="POST">
+            <input type="text" name="vardas">
+            <input type="submit">
         </form>
     </body>
 </html>
