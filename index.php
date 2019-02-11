b69e140bf0b058a3eda033c11afe48e60f20755c
donatasju <donatas.jurkusl@gmail.com>
2019-02-11 11:56:55 +0200

    form array

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
 <?php
-$input =  filter_input_array(INPUT_POST, [
-    'vardas' => FILTER_SANITIZE_SPECIAL_CHARS,
-        ]);
+$array = [
+    'input' => [
+        [
+            'text' => 'Mano vardas',
+            'type' => 'text',
+            'id' => 'name',
+            'placeholder' => 'Vardas',
+        ],
+        [
+            'text' => 'Kiek turiu zirniu ?',
+            'type' => 'text',
+            'id' => 'name',
+            'placeholder' => '1-100',
+        ],
+        [
+            'text' => 'Paslaptis, kodel turiu zirniu',
+            'type' => 'password',
+            'id' => 'reason',
+            'placeholder' => 'Issipasakok',
+        ]
+    ],
+    'button' => [
+        [
+            'text' => 'Paberti...',
+            'name' => 'action',
+            'value' => 'do_zirniai',
+        ]
+    ]
+];
 ?>
 <html>
     <head>
@@ -9,11 +35,18 @@ $input =  filter_input_array(INPUT_POST, [
         <link rel="stylesheet" href="css/style.css"/>
     </head>
     <body>
-        <h1><?php print $input['vardas'] ?? ''; ?></h1>
+        <h1></h1>
         <h2>Hack this page</h2>
         <form method="POST">
-            <input type="text" name="vardas">
-            <input type="submit">
+            <label>
+                <?php foreach ($array['input'] as $input): ?>
+                    <span><?php print $input['text']; ?></span>
+                    <input type="<?php print $input['type']; ?>" name="<?php print $input['id']; ?>" placeholder="<?php print $input['placeholder']; ?>">
+                <?php endforeach; ?>
+            </label>
+            <?php foreach ($array['button'] as $button): ?>
+                <button name="<?php print $button['name']; ?>" value="<?php print $button['value']; ?>"><?php print $button['text']; ?></button>
+            <?php endforeach; ?>
         </form>
     </body>
 </html>
