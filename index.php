c344e7ac28581fef430be653aa907cae01c6ceb4
Katamaranas <ernestas.zidokas@gmail.com>
2019-02-11 11:50:12 +0200

    Forma spausdinam su Array

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
 <?php
-$input = filter_input_array(INPUT_POST, [
-    'vardas' => FILTER_SANITIZE_SPECIAL_CHARS,
-        ]);
+$array = [
+    'input' => [
+        [
+            'type' => 'text',
+            'name' => 'name',
+            'placeholder' => 'Vardas',
+            'label' => 'Mano Vardas'
+        ],
+        [
+            'type' => 'text',
+            'name' => 'zirniai',
+            'placeholder' => '1-100',
+            'label' => 'Kiek turiu zirniu?'
+        ],
+        [
+            'name' => 'action',
+            'placeholder' => 'issipasakok',
+            'label' => 'Paslaptis',
+            'type' => 'password',
+        ],
+    ],
+    'button' => [
+        [
+            'name' => 'action',
+            'value' => 'do_zirniai',
+            'label' => 'paberti'
+        ]
+    ]
+];
 ?>
 <html>
     <head>
@@ -9,11 +35,18 @@ $input = filter_input_array(INPUT_POST, [
         <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
-        <h1><?php print $input['vardas'] ?? ''; ?></h1>
-        <h2>Hack this page</h2>
         <form method="POST">
-            <input type="text" name="vardas">
-            <input type="submit">
+            <?php foreach ($array['input'] as $input): ?>
+                <label>
+                    <span><?php print $input['label']; ?></span>
+                    <input type="<?php print $input['type']; ?>" 
+                           name="<?php print $input['name']; ?>" 
+                           placeholder="<?php print $input['placeholder']; ?>">
+                </label>
+            <?php endforeach; ?>
+            <?php foreach ($array['button'] as $value): ?>
+                <button name="<?php print $value['name']; ?>" value="<?php print $value['value']; ?>"><?php print $value['label']; ?></button>
+            <?php endforeach; ?>
         </form>
     </body>
 </html>
