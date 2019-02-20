f2331197c9939377c4c38d712dffafdaac81343d
Katamaranas <ernestas.zidokas@gmail.com>
2019-02-20 11:55:47 +0200

    pizdaball create + join commit + cookies DAINIAUS + play + SERVER SIDE COOKIEs + scoreboard + individual score

--- scoreboard.php
+++ scoreboard.php
@@ -7,10 +7,10 @@ if (file_exists(STORAGE_FILE)) {
     $team_idx = $_SESSION['team'] ?? false;
     $nick = $_SESSION['nick'] ?? false;
     $teams_array = file_to_array(STORAGE_FILE);
-    
+
     foreach ($teams_array[$team_idx]['players'] as $player) {
         $individual_score = 0;
-        
+
         if ($player['nick_name'] == $nick) {
 
             $individual_score = $player['score'];
@@ -41,8 +41,9 @@ if (file_exists(STORAGE_FILE)) {
         <h1>PZDABALL Scoreboard!</h1>
         <?php foreach ($teams_array as $team): ?>
             <p><?php print 'Komandos pavadinimas: ' . $team['team_name']; ?></p>
-            <p><?php print 'Komandos score: ' . $team['team_score']; ?></p
+            <p><?php print 'Komandos score: ' . $team['team_score']; ?></p>
         <?php endforeach; ?>
+            <h3>Zaidejo duomenys:</h3>
         <p><?php print 'Tavo nick: ' . $nick ?></p>
         <p><?php print 'Tavo individual score: ' . $individual_score; ?></p>
     </body>
