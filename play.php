0a834e5f6df65a7b2bbccd1e0920c48fad194056
Katamaranas <ernestas.zidokas@gmail.com>
2019-02-20 11:05:49 +0200

    pizdaball create + join commit + cookies DAINIAUS + play + SERVER SIDE COOKIEs + scoreboard

--- play.php
+++ play.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'bootstrap.php';
+
 session_start();
 
 $form = [
--- scoreboard.php
+++ scoreboard.php
@@ -1,7 +1,19 @@
 <?php
 require_once 'bootstrap.php';
 
-// TO DO
+
+if (file_exists(STORAGE_FILE)) {
+    $teams_array = file_to_array(STORAGE_FILE);
+    foreach ($teams_array as &$team) {
+        $team['team_score'] = 0;
+        var_dump($team['team_name']);
+        foreach ($team['players'] as $player) {
+            $team['team_score'] += $player['score'];
+        }
+    }
+    unset($team);
+    array_to_file($teams_array, STORAGE_FILE);
+}
 ?>
 <html>
     <head>
@@ -14,5 +26,9 @@ require_once 'bootstrap.php';
 
         <!-- Content -->       
         <h1>PZDABALL Scoreboard!</h1>
+        <?php foreach ($teams_array as $team): ?>
+            <p><?php print $team['team_name']; ?></p>
+            <p><?php print $team['team_score']; ?></p>
+        <?php endforeach; ?>
     </body>
 </html>
