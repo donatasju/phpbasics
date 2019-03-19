9745e0af85822ddc2385d026cd9f226665b7d939
donatasju <donatas.jurkusl@gmail.com>
2019-03-19 11:58:26 +0200

    first122

--- index.php
+++ index.php
@@ -4,16 +4,22 @@ class ThailandSurpise {
 
     public $clothes;
     private $balls;
-    
+
     public function __construct() {
         $this->balls = rand(0, 1);
+        $this->clothes = $clothes;
     }
-
+    
+    public function attachBalls() {
+        $this->balls = true;
+    }
+    
+    public function detachBalls() {
+        $this->balls = false;
+    }
 }
 
 $miniskirt = new ThailandSurpise();
 
-$miniskirt->clothes = 'miniskirt';
-print $miniskirt->clothes;
 
 
