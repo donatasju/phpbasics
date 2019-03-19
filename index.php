aaa3f1fe34c4c07230dbea7532d7061e289ee59c
donatasju <donatas.jurkusl@gmail.com>
2019-03-19 13:50:21 +0200

    done

--- index.php
+++ index.php
@@ -1,44 +1,55 @@
 <?php
 
-class ThailandSurpise {
+class FileDb {
 
-    public $clothes;
-    private $balls;
-    private $name;
+    private $file_uri;
+    private $data;
 
-    public function __construct($name) {
-        $this->balls = rand(0, 1);
-        $this->name = $name;
+    public function __construct($file_uri) {
+        $this->file_uri = $file_uri;
+        $this->data = null;
+        $this->load();
     }
 
-    public function attachBalls() {
-        $this->balls = true;
+    public function setRow($table, $row_id, $row_data) {
+        $this->data[$table][$row_id] = $row_data;
+    }
+    public function setRowColumn($table, $row_id, $column_id, $row_data) {
+        $this->data[$table][$row_id][$column_id] = $row_data;
+    }
+    
+    public function getRow($table, $row_id) {
+        return $this->data[$table][$row_id];
+    }
+    public function getRowColumn($table, $row_id, $column_id) {
+        return $this->data[$table][$row_id][$column_id];
     }
 
-    public function detachBalls() {
-        $this->balls = false;
-    }
-
-    public function getPhoto() {
-        if ($this->balls) {
-            return 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Logo-TRUE.svg/1200px-Logo-TRUE.svg.png';
+    public function load() {
+        if (!file_exists($this->file_uri)) {
+            $this->data = [];
         } else {
-            return 'https://1islam.files.wordpress.com/2012/08/false.jpg';
+            $json_data = file_get_contents($this->file_uri);
+            $this->data = json_decode($json_data, true);
+        }
+    }
+
+    public function save() {
+        $data_json = json_encode($this->data);
+        if (file_put_contents($this->file_uri, $data_json)) {
+            return true;
+        } else {
+            throw new Exception('Neisejo issaugoti i faila.');
         }
     }
 
 }
 
-$surpise = new ThailandSurpise('Donatas');
+$db = new FileDb('db/database.txt');
+$db->setRow('Donatas table named', 'row_id', ['location' => 'oral']);
+$db->save();
+
 ?>
-<html>
-    <head>
-        <title>First day of OOP</title>
-    </head>
-    <body>
-        <img src= "<?php print $surpise->getPhoto(); ?>" >
-    </body>
-</html>
 
 
 
