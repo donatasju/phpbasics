4c07ed0008b776dfa69130c243df3fabd3f840de
donatasju <donatas.jurkusl@gmail.com>
2019-03-19 12:25:26 +0200

    vienas

--- index.php
+++ index.php
@@ -8,19 +8,35 @@ class ThailandSurpise {
     public function __construct() {
         $this->balls = rand(0, 1);
     }
-    
+
     public function attachBalls() {
         $this->balls = true;
     }
-    
+
     public function detachBalls() {
         $this->balls = false;
     }
+
+    public function getPhoto() {
+        if ($this->balls == true) {
+            return 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Logo-TRUE.svg/1200px-Logo-TRUE.svg.png';
+        } else {
+            return 'https://1islam.files.wordpress.com/2012/08/false.jpg';
+        }
+    }
+
 }
 
 $surpise = new ThailandSurpise();
+?>
+<html>
+    <head>
 
-$surpise->clothes = 'miniskirt';
+    </head>
+    <body>
+        <img src=<?php print $surpise->getPhoto() ?>>
+    </body>
+</html>
 
 
 
