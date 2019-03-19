a77b49d527085ee64f5588c677589b0b4bec9a81
donatasju <donatas.jurkusl@gmail.com>
2019-03-19 12:33:23 +0200

    vienas23

--- index.php
+++ index.php
@@ -18,11 +18,9 @@ class ThailandSurpise {
     }
 
     public function getPhoto() {
-        if ($this->balls == true) {
-            
+        if ($this->balls) {
             return 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Logo-TRUE.svg/1200px-Logo-TRUE.svg.png';
         } else {
-            
             return 'https://1islam.files.wordpress.com/2012/08/false.jpg';
         }
     }
@@ -33,10 +31,10 @@ $surpise = new ThailandSurpise();
 ?>
 <html>
     <head>
-
+        <title>First day</title>
     </head>
     <body>
-        <img src= <?php print $surpise->getPhoto(); ?> >
+        <img src= "<?php print $surpise->getPhoto(); ?>" >
     </body>
 </html>
 
