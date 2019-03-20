8f07d256306ec0cc730651f5d212d6017f17beb2
donatasju <donatas.jurkusl@gmail.com>
2019-03-20 10:46:13 +0200

    vienas2

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ class Jacuzzi {
     public $amount_non_water;
     
     public function __construct($amount_water = 0 , $amount_non_water = 0) {
-
+        $this->amount_water = $amount_water;
+        $this->amount_non_water = $amount_non_water;
     }
     
     
