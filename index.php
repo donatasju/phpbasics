d8b6374214f408fff45bd65f26388ec6f80587e8
donatasju <donatas.jurkusl@gmail.com>
2019-03-20 11:19:30 +0200

    dd

--- index.php
+++ index.php
@@ -5,9 +5,17 @@ class Jacuzzi {
     public $amount_water;
     public $amount_non_water;
 
-    public function __construct($amount_water = 0, $amount_non_water = 0) {
-        $this->amount_water = $amount_water;
-        $this->amount_non_water = $amount_non_water;
+    public function __construct($water = 0, $non_water = 0) {
+        $this->amount_water = $water;
+        $this->amount_non_water = $non_water;
+    }
+    
+    public function getWaterPurity() {
+        return $this->amount_water / ($this->amount_water + $this->amount_non_water) * 100;
+        
     }
     
 }
+
+$skaidruu = new Jacuzzi(1000, 100);
+print $skaidruu->getWaterPurity();
